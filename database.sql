2259b4e888fee20c880303f4e30515f216cfb3ca
Anna Smertina <smr.anya@gmail.com>
2024-06-24 14:24:44 +0100

    Create DB, add deps

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id bigint,
+    name varchar(255),
+    created_at timestamp
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 import os
+import psycopg2
 from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route('/')
@@ -12,3 +15,4 @@ def hello_world():
     return render_template(
         'main.html'
     )
+
