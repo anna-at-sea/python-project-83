4badf14e216c6569b008830906294a17c05aa566
Anna Smertina <smr.anya@gmail.com>
2024-06-29 14:40:08 +0100

    Add validator

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
 import os
-import datetime
+from datetime import date
 import psycopg2
-from flask import Flask, render_template, request, make_response, url_for, redirect, get_flashed_messages, flash
+from validators.url import url
+from urllib.parse import urlparse
+from flask import Flask, render_template, request, make_response, \
+    url_for, redirect, get_flashed_messages, flash
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -16,17 +20,40 @@ cur = conn.cursor()
 def main_page():
     messages = get_flashed_messages(with_categories=True)
     entry = {'name': ''}
+    all_entries = get_all_urls()
     return render_template(
         'main.html',
         messages=messages,
-        entry=entry
+        entry=entry,
+        all_entries=all_entries
     )
+
 
 @app.post('/')
 def add_entry():
-    new_entry = request.form.to_dict()
-    cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (new_entry['url'], datetime.datetime.now()))
-    conn.commit()
+    new_entry = validate_and_normalize(request.form.to_dict()['url'])
+    if not new_entry:
+        flash('Incorrect URL', 'error')
+    elif new_entry in get_all_urls():
+        flash('URL already exists', 'warning')
+    else:
+        cur.execute(
+            "INSERT INTO urls (name, created_at) VALUES (%s, %s)",
+            (new_entry, date.today())
+        )
+        conn.commit()
+        flash('Website was added successfully', 'success')
     response = make_response(redirect(url_for('main_page'), code=302))
-    flash('Website was added successfully', 'success')
     return response
+
+
+def validate_and_normalize(url_str):
+    if url(url_str) and len(url_str) <= 255:
+        parsed_url = urlparse(url_str)
+        return f'{parsed_url.scheme}://{parsed_url.netloc}'
+
+
+def get_all_urls():
+    cur.execute("SELECT name FROM urls;")
+    all_entries = cur.fetchall()
+    return [entry[0] for entry in all_entries]
