98f6cd8365e52b2a117fce9946474391416a2cd9
Anna Smertina <smr.anya@gmail.com>
2024-06-30 14:51:12 +0100

    Add url_page.html with processor

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -34,6 +34,7 @@ def add_entry():
     new_entry = validate_and_normalize(request.form.to_dict()['url'])
     if not new_entry:
         flash('Incorrect URL', 'error')
+        return make_response(redirect(url_for('main_page'), code=302))
     elif new_entry in get_all_urls():
         flash('URL already exists', 'warning')
     else:
@@ -43,8 +44,30 @@ def add_entry():
         )
         conn.commit()
         flash('Website was added successfully', 'success')
-    response = make_response(redirect(url_for('main_page'), code=302))
+    cur.execute("SELECT id FROM urls WHERE name = %s", (new_entry,))
+    new_entry_id = cur.fetchall()[0][0]
+    response = make_response(
+        redirect(url_for('url_page', url_id=new_entry_id), code=302)
+    )
     return response
+
+
+@app.route('/urls/<int:url_id>')
+def url_page(url_id):
+    messages = get_flashed_messages(with_categories=True)
+    cur.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+    current_url = cur.fetchall()
+    if not current_url:
+        return 'Page not found', 404
+    url_name = current_url[0][1]
+    url_date = current_url[0][2]
+    return render_template(
+        'url_page.html',
+        id=url_id,
+        name=url_name,
+        date=url_date,
+        messages=messages
+    )
 
 
 def validate_and_normalize(url_str):
