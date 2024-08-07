d0b25637e9c2b9612384ea799d1b899b7c6956fc
Anna Smertina <smr.anya@gmail.com>
2024-08-07 16:46:21 +0200

    Add context manager in db_manager.py

--- page_analyzer/db_manager.py
+++ page_analyzer/db_manager.py
@@ -7,10 +7,14 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-def connect():
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    return conn, cur
+class Connection:
+
+    def __enter__(self):
+        self.conn = psycopg2.connect(DATABASE_URL)
+        return self.conn
+
+    def __exit__(self, type, value, traceback):
+        self.conn.close()
 
 
 def select_from_bd(table, columns=[], where={}, desc=False):
@@ -26,43 +30,48 @@ def select_from_bd(table, columns=[], where={}, desc=False):
     order_by = ' ORDER BY id DESC' if desc else ''
     query_without_where = f"SELECT {columns_to_str.strip(' ,')} \
     FROM {table}{where_to_str}{order_by};"
-    _, cur = connect()
-    if not val:
-        cur.execute(query_without_where)
-    else:
-        cur.execute(query_without_where, val)
-    return cur.fetchall()
+    with Connection() as connection:
+        cur = connection.cursor()
+        if not val:
+            cur.execute(query_without_where)
+        else:
+            cur.execute(query_without_where, val)
+        return cur.fetchall()
 
 
 def add_url(name):
-    conn, cur = connect()
-    cur.execute("INSERT INTO urls (name) VALUES (%s)", (name,))
-    conn.commit()
+    with Connection() as connection:
+        cur = connection.cursor()
+        cur.execute("INSERT INTO urls (name) VALUES (%s)", (name,))
+        connection.commit()
 
 
 def add_url_check(*values):
-    conn, cur = connect()
-    cur.execute(
-        "INSERT INTO url_checks (url_id, status_code, h1, title, description) \
-        VALUES (%s, %s, %s, %s, %s)", (values)
-    )
-    conn.commit()
+    with Connection() as connection:
+        cur = connection.cursor()
+        cur.execute(
+            "INSERT INTO url_checks \
+            (url_id, status_code, h1, title, description) \
+            VALUES (%s, %s, %s, %s, %s)", (values)
+        )
+        connection.commit()
 
 
 def get_all_urls_table():
-    _, cur = connect()
-    cur.execute(
-        "WITH filtered_checks \
-        AS (\
-        SELECT url_id, MAX(DATE(created_at)) AS created_at, status_code \
-        FROM url_checks \
-        GROUP BY url_id, status_code\
-        ) \
-        SELECT urls.id, urls.name, filtered_checks.created_at, \
-        filtered_checks.status_code \
-        FROM urls \
-        LEFT JOIN filtered_checks \
-        ON urls.id = filtered_checks.url_id \
-        ORDER BY id DESC;"
-    )
-    return cur.fetchall()
+    with Connection() as connection:
+        cur = connection.cursor()
+        cur.execute(
+            "WITH filtered_checks \
+            AS (\
+            SELECT url_id, MAX(DATE(created_at)) AS created_at, status_code \
+            FROM url_checks \
+            GROUP BY url_id, status_code\
+            ) \
+            SELECT urls.id, urls.name, filtered_checks.created_at, \
+            filtered_checks.status_code \
+            FROM urls \
+            LEFT JOIN filtered_checks \
+            ON urls.id = filtered_checks.url_id \
+            ORDER BY id DESC;"
+        )
+        return cur.fetchall()
